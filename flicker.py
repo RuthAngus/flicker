04872deded86e557b7345b10814064aa8d9a4373
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-04-08 15:27:19 -0400

    reversing axes

--- flicker.py
+++ flicker.py
@@ -21,20 +21,21 @@ def make_inverse_flicker_plot(x, xerr, y, yerr, samples, plot_samp=False):
     print alpha, beta, tau, sigma
     ys = alpha + beta * x
     sig_tot = sigma + yerr
+    ys = np.linspace(min(y)-5, max(y)+5, 1000)
     xs = np.linspace(min(x), max(x), 1000)
     plt.clf()
     # intrinsic scatter
-    plt.fill_between(xs, alpha+beta*xs-sigma, alpha+beta*xs+sigma,
+    plt.fill_between(ys, ((ys-alpha)/beta)-sigma, ((ys-alpha)/beta)+sigma,
                      color=cols.blue, alpha=.3, edgecolor="w")
-    plt.fill_between(xs, alpha+beta*xs-2*sigma, alpha+beta*xs+2*sigma,
+    plt.fill_between(ys, ((ys-alpha)/beta)-2*sigma, ((ys-alpha)/beta)+2*sigma,
                      color=cols.blue, alpha=.2, edgecolor="w")
-    plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="k.", capsize=0, alpha=.5,
+    plt.errorbar(y, x, xerr=yerr, yerr=xerr, fmt="k.", capsize=0, alpha=.5,
                  ecolor=".5", mec=".2")
-    plt.plot(xs, alpha+beta*xs, ".2", linewidth=1)
-    plt.xlim(min(xs), max(xs))
-    plt.ylim(.5, 2.5)
-    plt.xlabel("$\log_{10}(\\rho_{star}[\mathrm{kgm}^{-3}])$")
-    plt.ylabel("$\log_{10}\mathrm{(8-hour~flicker~[ppm])}$")
+    plt.plot(ys, (ys-alpha)/beta, ".2", linewidth=1)
+    plt.ylim(.5, 4.)
+    plt.xlim(1, 2.4)
+    plt.ylabel("$\log_{10}(\\rho_{star}[\mathrm{kgm}^{-3}])$")
+    plt.xlabel("$\log_{10}\mathrm{(8-hour~flicker~[ppm])}$")
     plt.savefig("flicker3")
 
 def make_flicker_plot(x, xerr, y, yerr, samples, plot_samp=False):
@@ -69,16 +70,6 @@ def make_flicker_plot(x, xerr, y, yerr, samples, plot_samp=False):
         plt.fill_between(xs, alpha+beta*xs-2*sigma, alpha+beta*xs+2*sigma,
                          color=cols.blue, alpha=.2, edgecolor="w")
 
-#         # intrinsic scatter + individual uncertainties, 1 sigma
-#         ys1 = interp(x, ys-sig_tot, xs, 1.3)
-#         ys2 = interp(x, ys+sig_tot, xs, 1.3)
-#         plt.fill_between(xs, ys1, ys2, color=cols.blue, alpha=.2, edgecolor="w")
-
-#         # intrinsic scatter + individual uncertainties, 2 sigma
-#         ys1 = interp(x, ys-2*sig_tot, xs, 5.2)
-#         ys2 = interp(x, ys+2*sig_tot, xs, 5.2)
-#         plt.fill_between(xs, ys1, ys2, color=cols.blue, alpha=.1, edgecolor="w")
-
     plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="k.", capsize=0, alpha=.5,
                  ecolor=".5", mec=".2")
     plt.plot(xs, alpha+beta*xs, ".2", linewidth=1)
@@ -91,6 +82,14 @@ def make_flicker_plot(x, xerr, y, yerr, samples, plot_samp=False):
         plt.savefig("flicker2")
     else:
         plt.savefig("flicker")
+
+    resids = y - (alpha+beta*x)
+    normed_resids = resids / np.sqrt(yerr**2 + sigma**2)
+    plt.clf()
+    plt.hist(normed_resids, 20, histtype="stepfilled", color="w")
+    plt.xlabel("Normalised residuals")
+    print np.std(normed_resids)
+    plt.savefig("residual_hist")
 
 if __name__ == "__main__":
 
@@ -111,7 +110,4 @@ if __name__ == "__main__":
     tau_chain = np.concatenate((c1[20001:30000], c2[20001:30000]))
     samples = np.vstack((alpha_chain, beta_chain, tau_chain))
     make_flicker_plot(x, xerr, y, yerr, samples)
-
-    with h5py.File("../../emcee_samples.h5", "r") as f:
-        samples = f["samples"][:, :]
-#     make_flicker_plot(x, xerr, y, yerr, samples.T)
+    make_inverse_flicker_plot(x, xerr, y, yerr, samples)
