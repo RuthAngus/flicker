9f4f134481e98b76cd1c74826e73583bba1b102d
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-05-12 13:41:55 -0400

    now with mixture model

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from noisy_plane import generate_samples, lnlike, lnlikeH
+from noisy_plane import generate_samples, lnlike, lnlikeH, lnlikeHM
 from model import model1, model
 import emcee
 import triangle
@@ -8,16 +8,19 @@ import sys
 import h5py
 
 def lnprior(pars):
-    return 0.
+    m, c, sig, Y, V, P = pars
+    if 0. < P < 1. and 0. < V:
+        return 0.
+    return -np.inf
 
 def lnprob(pars, samples, obs, u):
-    return lnlikeH(pars, samples, obs, u) + lnprior(pars)
+    return lnlikeHM(pars, samples, obs, u) + lnprior(pars)
 
 def MCMC(whichx, nsamp):
 
-    # set initial params
-    rho_pars = [-2., 6., .0065]
-    logg_pars = [-1.850, 7., .0065]
+    # set initial params. slope, intercept, sigma, Y, V, P
+    rho_pars = [-2., 6., .0065, -.5, .5, .1]
+    logg_pars = [-1.850, 7., .0065, 4., 1., .1]
     pars_init = logg_pars
     if whichx == "rho":
         pars_init = rho_pars
@@ -35,14 +38,6 @@ def MCMC(whichx, nsamp):
     u = np.vstack((xerr, yerr))
     s = generate_samples(obs, u, nsamp)
 
-#     plt.clf()
-#     x, y = obs[0, :], obs[1, :]
-#     xerr, yerr = u[0, :], u[1, :]
-#     plt.plot(s[0, :, :], s[1, :, :], "r.", markersize=2, alpha=.3, zorder=0)
-#     plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="ko", capsize=0, zorder=1)
-#     plt.show()
-#     assert 0
-
     # set up and run emcee
     ndim, nwalkers = len(pars_init), 32
     pos = [pars_init + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
@@ -54,7 +49,7 @@ def MCMC(whichx, nsamp):
     print "production run..."
     sampler.run_mcmc(pos, 1000)
     samp = sampler.chain[:, 50:, :].reshape((-1, ndim))
-    m, c, sig = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+    m, c, sig, Y, V, P, = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                zip(*np.percentile(samp, [16, 50, 84], axis=0)))
     pars = [m[0], c[0], sig[0]]
 
@@ -65,10 +60,12 @@ def MCMC(whichx, nsamp):
     data[:, 0] = samp[:, 0]
     data[:, 1] = samp[:, 1]
     data[:, 2] = samp[:, 2]
+    data[:, 3] = samp[:, 3]
+    data[:, 4] = samp[:, 4]
+    data[:, 5] = samp[:, 5]
     f.close()
-    return s
 
-def make_plots(whichx, s):
+def make_plots(whichx):
 
     # load data
     fr, frerr, r, rerr = np.genfromtxt("../data/flickers.dat").T
@@ -80,13 +77,12 @@ def make_plots(whichx, s):
 
     with h5py.File("%s_samples.h5" % whichx) as f:
         samp = f["samples"][...]
-    m, c, sig = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+    m, c, sig, Y, V, P = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                zip(*np.percentile(samp, [16, 50, 84], axis=0)))
     pars = [m[0], c[0], sig[0]]
 
     plt.clf()
     plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="k.", capsize=0, ecolor=".7")
-    plt.plot(s[0, :, :], s[1, :, :], "r.", markersize=2)
     plt.plot(x, model1(pars, x), "k")
     ndraws = 100
     p1s = np.random.choice(samp[:, 0], ndraws)
@@ -94,10 +90,11 @@ def make_plots(whichx, s):
     for i in range(ndraws):
         plt.plot(x, model1([p1s[i], p2s[i]], x), "k", alpha=.1)
     plt.savefig("mcmc_%s" % whichx)
-    fig = triangle.corner(samp, truths=pars)
+    labels = ["$m$", "$c$", "$\sigma$", "$Y$", "$V$", "$f$"]
+    fig = triangle.corner(samp, labels=labels)
     fig.savefig("triangle_%s" % whichx)
 
 if __name__ == "__main__":
     whichx = str(sys.argv[1])
-    s = MCMC(whichx, 50)
-    make_plots(whichx, s)
+#     MCMC(whichx, 50)
+    make_plots(whichx)
