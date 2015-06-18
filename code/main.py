f1d4fc886f0a4d30ec4bcdcea746e88d221559ac
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-06-18 13:42:57 +0000

    plotting outlying data

--- code/main.py
+++ code/main.py
@@ -30,10 +30,13 @@ def MCMC(whichx, nsamp, fname):
     # load data
     data = np.genfromtxt("../data/BIGDATA.filtered.dat").T
     r, rerrp, rerrm = data[7:10]
-    f, ferr = data[20:22]
-    logg, loggerrp, loggerrm = data[10:13]
     rerrp, rerrm = rerrp/r/np.log(10), rerrm/r/np.log(10)
     r = np.log10(1000*r)
+    data2 = np.genfromtxt("../data/BIGDATA.nohuber.filtered.dat").T
+    logg, loggerrp, loggerrm = data2[10:13]
+    f, ferr = data2[20:22]
+    if whichx == "rho":
+        f, ferr = data[20:22]
     ferrp, ferrm = ferr/f/np.log(10), ferr/f/np.log(10)
     f = np.log10(1000*f)
 
@@ -47,13 +50,14 @@ def MCMC(whichx, nsamp, fname):
     yerr = .5*(yerrp + yerrm)
     if whichx == "rho":
         m = np.isfinite(r[:nd])
-        x, xerrp, xerrm = f[:nd][m], ferrp[:nd][m], ferrm[:nd][m]
+        x, xerrp, xerrm = f[:nd][m]-3, ferrp[:nd][m], ferrm[:nd][m]
         y, yerrp, yerrm = r[:nd][m], rerrp[:nd][m], rerrm[:nd][m]
         xerr = .5*(xerrp + xerrm)
         yerr = .5*(yerrp + yerrm)
 
     plt.clf()
-    plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="k.")
+#     plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="k.")
+    plt.plot(x, y, "k.")
 
     # load data
     fr, frerr, r, rerr = np.genfromtxt("../data/flickers.dat").T
@@ -64,7 +68,8 @@ def MCMC(whichx, nsamp, fname):
     if whichx == "rho":
         x, xerr, y, yerr = fr[:nd], frerr[:nd], r[:nd], rerr[:nd]
 
-    plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="r.", alpha=.5)
+#     plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="r.", alpha=.5)
+    plt.plot(x, y, "r.", alpha=.5)
     plt.savefig("test")
     assert 0
 
@@ -156,5 +161,95 @@ def make_plots(whichx, fname):
 
 if __name__ == "__main__":
     whichx = str(sys.argv[1])
-    MCMC(whichx, 10, "newdata")
-    make_plots(whichx, "newdata")
+#     MCMC(whichx, 10, "newdata")
+#     make_plots(whichx, "newdata")
+
+    # load data
+    data = np.genfromtxt("../data/BIGDATA.filtered.dat").T
+    kid = data[0]
+    all_mass = data[1]
+    r, rerrp, rerrm = data[7:10]
+    rerrp, rerrm = rerrp/r/np.log(10), rerrm/r/np.log(10)
+    r = np.log10(1000*r)
+    data2 = np.genfromtxt("../data/BIGDATA.nohuber.filtered.dat").T
+#     data2 = np.genfromtxt("../data/BIGDATA.filtered.dat").T
+    kid_no_huber = data2[0]
+    logg, loggerrp, loggerrm = data2[10:13]
+    f, ferr = data2[20:22]
+
+#     plt.clf()
+#     plt.plot(f, r, "k.")
+#     plt.plot(f, logg, "r.")
+#     plt.savefig("bastien_comparison")
+
+    if whichx == "rho":
+        f, ferr = data[20:22]
+    ferrp, ferrm = ferr/f/np.log(10), ferr/f/np.log(10)
+    f = np.log10(1000*f)
+
+    # format data
+    nd = len(f)
+    m = np.isfinite(logg[:nd])
+    x, xerrp, xerrm = f[:nd][m]-3, ferrp[:nd][m], ferrm[:nd][m]
+    y, yerrp, yerrm = logg[:nd][m], loggerrp[:nd][m], loggerrm[:nd][m]
+    xerr = .5*(xerrp + xerrm)
+    yerr = .5*(yerrp + yerrm)
+    if whichx == "rho":
+        m = np.isfinite(r[:nd])
+        kid = kid[:nd][m]
+        all_mass = all_mass[:nd][m]
+        x, xerrp, xerrm = f[:nd][m]-3, ferrp[:nd][m], ferrm[:nd][m]
+        y, yerrp, yerrm = r[:nd][m], rerrp[:nd][m], rerrm[:nd][m]
+        xerr = .5*(xerrp + xerrm)
+        yerr = .5*(yerrp + yerrm)
+
+    # just huber stars
+    huber, mass, teff = np.genfromtxt("../data/huber/TableY.dat",
+                                      usecols=(0,1,16)).T
+    hub, hf, hferr, hr, hrerr, hteff, hmass = [], [], [], [], [], [], []
+    for i, k in enumerate(huber):
+        m = kid == k
+        if len(kid[m]):
+            hteff.append(teff[i])
+            hmass.append(mass[i])
+            hub.append(kid[m][0])
+            hf.append(x[m][0])
+            hferr.append(xerr[m][0])
+            hr.append(y[m][0])
+            hrerr.append(yerr[m][0])
+
+    pkid, rpl, porb, prot = np.genfromtxt("../data/KOIrotation_periods.txt",
+                                          delimiter=",", skip_header=20,
+                                          usecols=(1,4,5,6)).T
+
+    hub = np.array(hub)
+    hf = np.array(hf)
+    hr = np.array(hr)
+    phub, phf, phr, phrpl, phporb, phprot = [], [], [], [], [], []
+    for i, k in enumerate(hub):
+        m = pkid == k
+        if len(pkid[m]):
+            phub.append(pkid[m][0])
+            phf.append(hf[i])
+            phr.append(hr[i])
+            phrpl.append(rpl[m][0])
+            phporb.append(porb[m][0])
+            phprot.append(prot[m][0])
+
+#     plt.clf()
+#     plt.hist(hmass, normed=True)
+#     plt.hist(all_mass, normed=True, alpha=.5, color="r")
+#     plt.savefig("huber_hist")
+    plt.clf()
+    plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="k.", zorder=0)
+#     plt.scatter(x, y, c=all_mass, vmin=.8, vmax=1.6, zorder=0, edgecolor=None)
+#     plt.scatter(hf, hr, c=hmass, vmin=.8, vmax=1.6, s=80, marker="^", zorder=1)
+#     plt.scatter(hf, hr, c=hmass, s=80, marker="^", zorder=1)
+    plt.errorbar(hf, hr, xerr=hferr, yerr=hrerr, fmt="k.", zorder=1)
+#     print phprot
+#     print phporb
+#     plt.scatter(hf, hr, c=np.log10(phporb), s=80, marker="^", zorder=1)
+    plt.xlabel("flicker")
+    plt.ylabel("density")
+#     plt.colorbar()
+    plt.savefig("huber_test")
