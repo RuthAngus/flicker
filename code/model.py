7f1bf5a5b29ca6e6bacefbb94ef2912d0757b68f
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-07-06 15:25:23 +0000

    adding deming regression?

--- code/model.py
+++ code/model.py
@@ -52,6 +52,10 @@ def load_data(whichx, nd=0, bigdata=False, huber=False):
 def model1(pars, x):
     return pars[0]*x + pars[1]
 
+def deming_model(pars, x, y):
+    b, a = pars
+    return (a*b + x + b*y) / (1 + b**2), (a + b*(x + b*y)) / (1 + b**2)
+
 def model(pars, samples):
     xs = samples[0, :, :]
     x = np.reshape(xs, np.shape(xs)[0]* np.shape(xs)[1])
@@ -82,6 +86,12 @@ def nll(pars, x, y, yerr, model):
     chi2 = -.5 * ((y-mod)**2 * inv_sig2)
     return - np.logaddexp.reduce(chi2, axis=0)
 
+def deming_nll(pars, x, y, yerr, model):
+    inv_sig2 = 1./(yerr**2 + xerr**2)
+    xc, yc = deming_model(pars, x, y)
+    chi2 = ((x-xc)**2 + (y-yc)**2) * inv_sig2
+    return - np.logaddexp.reduce(chi2, axis=0)
+
 if __name__ == "__main__":
 
     x, y, xerr, yerr = load_data("logg", bigdata=True)
@@ -92,18 +102,23 @@ if __name__ == "__main__":
 
     plt.clf()
     plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt="k.", capsize=0, ecolor=".7")
-    plt.xlim(1.6, 2.2)
-    plt.plot(x, broken_power_law1(res.x, x), "m")
+
+#     plt.xlim(1.6, 2.2)
+#     plt.plot(x, broken_power_law1(res.x, x), "m")
 
     pars_init = [5.6, -1]
     res = spo.minimize(nll, pars_init, args=(x, y, yerr, model1))
     print res.x
-
     plt.plot(x, model1(res.x, x), "y")
 
-    pars_init = [5.6, -1, .5, .5]
-    res = spo.minimize(nll, pars_init, args=(x, y, yerr, polynomial))
-    print res.x
+#     pars_init = [5.6, -1, .5, .5]
+#     res = spo.minimize(nll, pars_init, args=(x, y, yerr, polynomial))
+#     print res.x
+#     plt.plot(x, polynomial(res.x, x), "b")
 
-    plt.plot(x, polynomial(res.x, x), "b")
+    pars_init = [5.6, -1]
+    res = spo.minimize(deming_nll, pars_init, args=(x, y, yerr, deming_model))
+    print res.x
+    plt.plot(x, deming_model(res.x, x, y)[0], "m")
+
     plt.savefig("model")
