907f368022788467068fd84fece32a3419fbbecc
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-07-10 20:29:31 +0000

    adding mixture model

--- code/noisy_plane.py
+++ code/noisy_plane.py
@@ -92,6 +92,42 @@ def lnlikeHF(pars, samples, obs, u, extra=False):
         return loglike
     return -np.inf
 
+# n-D, hierarchical log-likelihood with sigma as a function of y
+def lnlikeHFM(pars, samples, obs, u, extra=False):
+    '''
+    Generic likelihood function for importance sampling with any number of
+    dimensions.
+    Now with added jitter parameter (hierarchical)
+    obs should be a 2d array of observations. shape = (ndims, nobs)
+    u should be a 2d array of uncertainties. shape = (ndims, nobs)
+    samples is a 3d array of samples. shape = (ndims, nobs, nsamp)
+    if extra == True, the sigma has both a slope and intercept
+    Now with a mixture model!
+    '''
+    ndims, nobs, nsamp = samples.shape
+    ypred = model(pars, samples)
+    yobs = obs[1, :]
+    xobs = obs[0, :]
+    yerr = u[1, :]
+    ll1 = np.zeros((nobs, nsamp*nobs))
+    ll2 = np.zeros((nobs, nsamp*nobs))
+    Y, V, P = pars[4], pars[5], pars[6]
+    for i in range(nobs):
+        if extra:
+            inv_sigma2 = 1.0/(yerr[i]**2 + \
+                    (pars[2] + pars[3] * model1(pars, xobs[i]))**2 + V)
+        else:
+            inv_sigma2 = 1.0/(yerr[i]**2 + \
+                    (pars[2]*model1(pars, xobs[i]))**2 + V)
+        ll1[i, :] = -.5*((yobs[i] - ypred)**2*inv_sigma2) + np.log(inv_sigma2)
+        ll2[i, :] = -.5*((yobs[i] - Y)**2*inv_sigma2) + np.log(inv_sigma2)
+    loglike1 = np.logaddexp.reduce(ll, axis=1)
+    loglike2 = np.logaddexp.reduce(ll, axis=1)
+    loglike = np.sum(np.logaddexp(np.log(1-P) + lnlike1, np.log(P) + lnlike2))
+    if np.isfinite(loglike):
+        return loglike
+    return -np.inf
+
 def generate_samples_log(obs, up, um, N):
     '''
     obs is a 2darray of observations. shape = (ndims, nobs)
