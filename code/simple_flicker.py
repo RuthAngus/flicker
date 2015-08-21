61394f23a82c50b90266557389c4da9bc4767536
RuthAngus <ruth.angus@astro.ox.ac.uk>
2015-08-21 18:22:25 +0000

    removing plots

--- code/simple_flicker.py
+++ code/simple_flicker.py
@@ -4,26 +4,14 @@ import emcee
 import triangle
 from model import load_data
 import sys
+import h5py
 
-# N = 25
-# m_true, b_true = -1.5, .25
-# lnsy_true = -1
-# xerr = np.random.uniform(.1, .2, N)
-# x0 = np.random.uniform(0, 5, N)
-# x = x0 + xerr * np.random.randn(N)
-# yerr = np.random.uniform(.1, .5, N)
-# y0 = m_true * x0 + b_true
-# y = y0 + np.sqrt(yerr**2 + np.exp(2*lnsy_true)) * np.random.randn(N)
-
+fname = "simple"
 whichx = str(sys.argv[1])
 x, y, xerr, yerr = load_data(whichx, nd=0, bigdata=True)
 m_init, b_init, lnsy_init = -2, 6, np.log(.0065)
 if whichx == "rho":
     m_init, b_init, lnsy_init = -1.85, 7., np.log(.0065)
-
-plt.clf()
-plt.errorbar(x, y, yerr=yerr, xerr=xerr, fmt="k.", capsize=0)
-plt.savefig("simple1")
 
 def lnlike((m, b, lns)):
     if not (-10 < lns < 5.):
@@ -36,14 +24,17 @@ sampler = emcee.EnsembleSampler(nwalkers, ndim, lnlike)
 
 p0 = np.array([m_init, b_init, lnsy_init]) \
         + 1e-5 * np.random.randn(nwalkers, ndim)
-p0, _, _ = sampler.run_mcmc(p0, 1000)
+p0, _, _ = sampler.run_mcmc(p0, 2000)
 sampler.reset()
-sampler.run_mcmc(p0, 1000)
+sampler.run_mcmc(p0, 5000)
 
-plt.clf()
-plt.plot(sampler.chain[:, :, 0].T, "k", alpha=.2)
-plt.savefig("simple2")
+triangle.corner(sampler.flatchain, truths=[-1.7, 2.013, np.log(.022)])
+plt.savefig("blah_logg")
 
-plt.clf()
-triangle.corner(sampler.flatchain, truths=[m_init, b_init, lnsy_init])
-plt.savefig("simple3")
+samp = sampler.chain[:, :, :].reshape(-1, ndim)
+f = h5py.File("%s_samples_%s.h5" % (whichx, fname), "w")
+data = f.create_dataset("samples", np.shape(samp))
+data[:, 0] = samp[:, 0]
+data[:, 1] = samp[:, 1]
+data[:, 2] = np.exp(samp[:, 2])
+f.close()
