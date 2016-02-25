4352a410156e21cc15bbdd62322f63ab9a6b191b
Ruth Angus <ruth.angus@astro.ox.ax.uk>
2016-02-25 17:19:13 +0000

    starting to add referee comments

--- code/triangle_plot.py
+++ code/triangle_plot.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import h5py
 import corner
 
-plotpar = {'axes.labelsize': 15,
-           'text.fontsize': 18,
-           'legend.fontsize': 18,
-           'xtick.labelsize': 15,
-           'ytick.labelsize': 15,
+plotpar = {'axes.labelsize': 19,
+           'text.fontsize': 19,
+           'legend.fontsize': 19,
+           'xtick.labelsize': 17,
+           'ytick.labelsize': 17,
            'text.usetex': True}
 plt.rcParams.update(plotpar)
 
@@ -16,6 +16,6 @@ with h5py.File("rho_samples.h5") as f:
 nwalkers, nsteps, ndim = np.shape(samples)
 flat = samples.reshape((nwalkers*nsteps, ndim))
 
-labels = ["$\\alpha$", "$\\beta$", "$\sigma$"]
+labels = ["$\\alpha_\\rho$", "$\\beta_\\rho$", "$\sigma_\\rho$"]
 fig = corner.corner(flat, labels=labels)
-plt.savefig("rho_triangle.pdf")
+plt.savefig("../version1.0/rho_triangle.pdf")
